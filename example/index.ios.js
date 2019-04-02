7752afc8ed824eba94bd9363dd1a803ffe137a9f
soufian97 <soufianelamrani29@gmail.com>
2019-04-02 11:34:20 +0100

    Update index.ios.js

--- example/index.ios.js
+++ example/index.ios.js
@@ -1,45 +1,62 @@
 import React from 'react';
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import {AppRegistry,Text,StyleSheet,Picker} from 'react-native';
+import {View} from 'react-native-webview';
 
-import {AzureInstance, AzureLoginView} from './azure';
-// The application registration (must match Azure AD config)
-var credentials = {
-  client_id: '2fa55851-89e0-4ccf-8b86-2a682bdb13d9',
-  client_secret: 'prJaOuSFMppLGa9E2bCScRR',
-  scope: 'User.ReadBasic.All User.Read.All User.ReadWrite.All Mail.Read offline_access'
+import {AzureInstance, AzureLoginView} from '../lib/';
+
+// CONSTANT
+const CREDENTIAILS = {
+    client_id: 'xxxxxxx-xxxxxxxxxx-xx-xxxx-xxxxxx',
+    client_secret: 'mlkjrzezlKH98ùùe)d',
+    scope: 'User.ReadBasic.All Mail.Read offline_access'
+    
 };
 
 export default class azureAuth extends React.Component {
 	constructor(props){
 		super(props);
-		this.azureInstance = new AzureInstance(credentials);
-
-		this._onLoginSuccess = this._onLoginSuccess.bind(this);
+    this.state = {
+      displayName : "Nan",
+      mail : "",
+      id : ""
+    };
+		this.azureInstance = new AzureInstance(CREDENTIAILS);
+    this._onLoginSuccess = this._onLoginSuccess.bind(this);
 	}
-
+	
 	_onLoginSuccess(){
 		this.azureInstance.getUserInfo().then(result => {
-			console.log(result);
+      console.log(result);
+    
+      
 		}).catch(err => {
 			console.log(err);
 		})
 	}
 
-
     render() {
         return (
-            <AzureLoginView
-            	azureInstance={this.azureInstance}
-            	loadingMessage="Requesting access token"
-            	onSuccess={this._onLoginSuccess}
-            />
+                <AzureLoginView
+                  azureInstance={this.azureInstance}
+                  loadingMessage="Requesting access token"
+                  onSuccess={this._onLoginSuccess}
+                />
         );
     }
+    
 }
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 23
+ },
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 AppRegistry.registerComponent('azureAuth', () => azureAuth);
